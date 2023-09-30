1d0687e35e8cb832284502514b8e49fb2b8e111e
kahuku <buzzinbubba@gmail.com>
2023-09-29 22:25:00 -0600

    initial commit

---
+++ visible files/boot.py
@@ -0,0 +1,4 @@
+from badusb.boot import Boot
+
+if __name__ == "__main__":
+    Boot()
---
+++ visible files/main.py
@@ -0,0 +1,4 @@
+from badusb.command import Command
+
+if __name__ == "__main__":
+    Command().execute("payload.txt")
